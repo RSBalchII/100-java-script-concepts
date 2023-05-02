e5ebc8418a7e7d3a3a306b844755ab0957d90498
RSBalchII <128771311+RSBalchII@users.noreply.github.com>
2023-05-02 13:46:06 -0600

    made develop branch and pushed  Please enter the commit message for your changes. Lines starting

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log('hello world')
+
+luckyNum = null
+
+luckyNumber = 23
+
+luckyNumber = 'twenty-three'
+
+let a = 'global'
+
+function fun (){
+    let a = 'function'
+
+    if (true) {
+        let a = 'block'
+        var b = 'hoisted'
+
+    }
+}
+console.log(a)
+
+//functions
+
+function ad1, b) {
+    return a + b
+}
+
+const add2 = function(a, b){
+    return a + b
+}
+
+
+
+
+//higher order functions
